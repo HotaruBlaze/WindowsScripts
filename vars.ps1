6891a8d6304f93a908ec285136e4a41a6a8d1ecb
MrFlutters <DeriousHD@gmail.com>
2021-03-26 16:09:36 +0000

    initial commit

---
+++ vars.ps1
@@ -0,0 +1,10 @@
+$installApplications = "Mozilla.Firefox","Discord.Discord","ShareX.ShareX","Microsoft.WindowsTerminal","Microsoft.VisualStudioCodeInsiders-User-x64","Valve.Steam","7zip.7zip","Microsoft.PowerToys"
+$FolderDrive = "D:\"
+$FolderPaths = @{
+    'Desktop'               = 'SYSTEM\Desktop';
+    'Documents'             = 'SYSTEM\Documents';
+    'Downloads'             = 'SYSTEM\Downloads';
+    'Pictures'              = 'SYSTEM\Pictures';
+    'Videos'                = 'SYSTEM\Videos';
+    'Music'                 = 'SYSTEM\Music';
+}
